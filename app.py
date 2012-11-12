93152fc4ca701731292ef5763dec2bd71f81e951
Kieran Darcy <kieranfdarcy@gmail.com>
2012-11-12 22:25:55 +0000

    Step 4: Add some more views and start working on the coffee form

--- app.py
+++ app.py
@@ -9,5 +9,17 @@ bootstrap = Bootstrap(app)
 def home():
     return render_template('home.html')
 
+@app.route('/brew-coffee/')
+def coffee():
+    return render_template('coffee.html')
+
+@app.route('/create-qrcode/')
+def create():
+    return render_template('create.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)

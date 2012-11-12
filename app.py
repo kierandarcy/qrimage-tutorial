0c0b59aa5f638d3c7933391c8388b37d8beb9e41
Kieran Darcy <kieranfdarcy@gmail.com>
2012-11-12 22:58:49 +0000

    Step 6: Introduce WTF Forms

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request, abort
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form, URL, Required
+from flask.ext.wtf.html5 import URLField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_very_secret_key'
 
 bootstrap = Bootstrap(app)
+
+# Forms
+class CreateForm(Form):
+    url = URLField(u'Enter a URL', validators=[Required(),
+                   URL(message=u'Please enter a well-formed URL')],
+                   description=u'Create a QR code from this URL')
 
 @app.route('/')
 def home():
@@ -15,9 +24,12 @@ def coffee():
         abort(418)
     return render_template('coffee.html')
 
-@app.route('/create-qrcode/')
+@app.route('/create-qrcode/', methods=['GET','POST'])
 def create():
-    return render_template('create.html')
+    form = CreateForm()
+    if form.validate_on_submit():
+        return render_template('create.html', form=form, show_last_image=True)
+    return render_template('create.html', form=form)
 
 @app.route('/login')
 def login():
